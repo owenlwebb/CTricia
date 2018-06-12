708ed6f02efbc15f420620875e3b67c9452b7bd5
Owen Webb <owebb@umich.edu>
2018-06-12 11:22:50 -0400

    Added testing file

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+*
+* Testing / Demonstration file for CTricia
+*
+* CTricia: A C++ Implementation of a PATRICIA tree for IP Address Operations
+* Author: Owen Webb - owebb@umich.edu
+*/
+
+#include <iostream>
+#include "ctricia.h"
+
+using namespace std;
+
+int main() {
+    CTricia trie;
+    return 0;
+}
